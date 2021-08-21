bd9b449d69d9235bdecd7e6545afe5d09189f3d3
Usman Suleiman <usmansbk@gmail.com>
2021-08-21 10:43:23 +0100

    Fetch countries action

--- src/redux/cases/cases.js
+++ src/redux/cases/cases.js
@@ -1,5 +1,22 @@
+const API_BASE = 'https://covid-api.mmediagroup.fr/v1/cases';
+
+const LOAD_COUNTRIES = 'cases/load-countries';
+
+const loadCountries = (payload) => ({
+  type: LOAD_COUNTRIES,
+  payload,
+});
+
+export const fetchCountries = (continent = 'Europe') => async (dispatch) => {
+  const response = await fetch(`${API_BASE}?continent=${continent}`);
+  const countries = await response.json();
+  dispatch(loadCountries(countries));
+};
+
 const reducer = (state = {}, action) => {
   switch (action.type) {
+    case LOAD_COUNTRIES:
+      return action.payload;
     default:
       return state;
   }
